5b7d1ccc08d61af1eea1f182fb65309fe50e0c4f
Lucas <lucaspraseldev@gmail.com>
2025-07-06 01:15:04 -0300

    sprite

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,12 +3,14 @@ import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
 import { defineConfig } from "vite";
+import VitePluginSvgSpritemap from "@spiriit/vite-plugin-svg-spritemap";
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
     tanstackRouter({}),
     react(),
+    VitePluginSvgSpritemap("./src/icons/*.svg"),
   ],
   resolve: {
     alias: {
